b9a067a12584539d5531d3fa5bbc6683a6753ae6
johanne101 <jlopez0493@gmail.com>
2021-12-23 10:54:50 +0000

    [Save] reviwed and it's correct

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,15 +1,9 @@
 -- script prepares a MySQL server for the project
 -- DB name:hbnb_test_db
 -- (1)user & (2)password(@'localhost'): (1)hbnb_test(@..), (2)hbnb_test_pwd
-<<<<<<< HEAD
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
 	IDENTIFIED BY 'hbnb_test_pwd';
-=======
-CREATE DATABASE IF NOT EXIST hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
-	IDENTIFIED BY 'hbnb_dev_pwd';
->>>>>>> johanne101
 
 -- (1)Granting All Privileges on hbnb_dev_db DB
 -- (2) Grant Select privilege on performance_schema
