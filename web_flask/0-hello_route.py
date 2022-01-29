cba23421507d7246423a9623827712040db4ae61
KevinTMO <kevin.ramos3@hotmail.com>
2022-01-29 18:41:27 +0000

    Confugring a flask web app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Module: 0-hello_route.py
+
+Write a script that starts a Flask web application
+
+1st part:
+
+- Import flask module
+- save Flask class with "__name__" in a var
+- Create a method named "whatever" with @.route decorator.
+  - Inside .route will be ('/'). You can use an entire route like ('/state')
+- Inside of the method return "Hello HBNB"
+
+2nd part:
+
+- Implement if "__main__" = "__name__"; so it can call itself
+- Enable the debugger so it can implement the configuration below.
+- use run from Flask to configure host/port:
+  - Configure to host 0.0.0.0
+  - Configure the port which the app will listen to.
+    - ex. port 5000
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def helloHBNB():
+    """
+    Return: Hello HBNB
+    """
+    return 'Hello HBNB'
+
+
+if "__main__" == "__name__":
+    """
+    Configuring host/port
+    """
+    app.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000, debug=True)
