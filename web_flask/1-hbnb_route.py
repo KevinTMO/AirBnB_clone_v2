e59f913e83f82301c7b89072d9abaa0eb0b8773d
KevinTMO <kevin.ramos3@hotmail.com>
2022-01-29 19:01:06 +0000

    Adding a route to app.route method decorator

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -27,12 +27,19 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/hbnb')
+@app.route('/')
 def helloHBNB():
     """
     Return: Hello HBNB
     """
     return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def HBNB():
+    """
+    Return: HBNB
+    """
+    return 'HBNB'
 
 
 if __name__ == "__main__":
