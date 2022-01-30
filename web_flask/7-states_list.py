ba922fcbf8090edb0670accc93144eb5c86e60a5
KevinTMO <kevin.ramos3@hotmail.com>
2022-01-30 15:40:15 +0000

    Display a list of states by a specific order and close the connection after requesting from app

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -22,19 +22,18 @@ Write a script that starts a Flask web application
     - ex. port 5000
 """
 
-from flask import Flask, request
+from flask import Flask, render_template
 from models import *
 from models import storage
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/states_list')
 def statesList():
     """
     Display a list of states in a specific order
     """
-    states = sorted(list(storage.all('State').values()), key=lambda x: x.name)
     return render_templates('7-states_list.html', states=states)
 
 
