00afc845551b80225f6899579b6c604f525c50d4
KevinTMO <kevin.ramos3@hotmail.com>
2022-02-02 14:27:02 +0000

    Adding a condition to validate the engine of my storage

--- models/state.py
+++ models/state.py
@@ -12,5 +12,6 @@ class State(BaseModel, Base):
     inherits from BaseModel and Base
     (respects the order)
     """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
